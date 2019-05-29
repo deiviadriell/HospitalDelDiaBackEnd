f6b2779657ae2140a98d5a77d6a857173855ab3e
deiviadriell <davidzunigaortiz92@gmail.com>
2019-05-29 11:50:44 -0500

    Archivos Controllers

---
+++ WebAPIHDD/Controllers/ArchivoControllers/EntidadsController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebAPIHDD;
+
+namespace WebAPIHDD.Controllers.ArchivoControllers
+{
+    public class EntidadsController : ApiController
+    {
+        private SMHospitalDelDiaEntities db = new SMHospitalDelDiaEntities();
+
+        // GET: api/Entidads
+        public IQueryable<Entidad> GetEntidad()
+        {
+            return db.Entidad;
+        }
+
+        // GET: api/Entidads/5
+        [ResponseType(typeof(Entidad))]
+        public IHttpActionResult GetEntidad(int id)
+        {
+            Entidad entidad = db.Entidad.Find(id);
+            if (entidad == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(entidad);
+        }
+
+        // PUT: api/Entidads/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutEntidad(int id, Entidad entidad)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != entidad.idEntidad)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(entidad).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EntidadExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Entidads
+        [ResponseType(typeof(Entidad))]
+        public IHttpActionResult PostEntidad(Entidad entidad)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Entidad.Add(entidad);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = entidad.idEntidad }, entidad);
+        }
+
+        // DELETE: api/Entidads/5
+        [ResponseType(typeof(Entidad))]
+        public IHttpActionResult DeleteEntidad(int id)
+        {
+            Entidad entidad = db.Entidad.Find(id);
+            if (entidad == null)
+            {
+                return NotFound();
+            }
+
+            db.Entidad.Remove(entidad);
+            db.SaveChanges();
+
+            return Ok(entidad);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool EntidadExists(int id)
+        {
+            return db.Entidad.Count(e => e.idEntidad == id) > 0;
+        }
+    }
+}
---
+++ WebAPIHDD/Controllers/ArchivoControllers/LinkArchivoesController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebAPIHDD;
+
+namespace WebAPIHDD.Controllers.ArchivoControllers
+{
+    public class LinkArchivoesController : ApiController
+    {
+        private SMHospitalDelDiaEntities db = new SMHospitalDelDiaEntities();
+
+        // GET: api/LinkArchivoes
+        public IQueryable<LinkArchivo> GetLinkArchivo()
+        {
+            return db.LinkArchivo;
+        }
+
+        // GET: api/LinkArchivoes/5
+        [ResponseType(typeof(LinkArchivo))]
+        public IHttpActionResult GetLinkArchivo(int id)
+        {
+            LinkArchivo linkArchivo = db.LinkArchivo.Find(id);
+            if (linkArchivo == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(linkArchivo);
+        }
+
+        // PUT: api/LinkArchivoes/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutLinkArchivo(int id, LinkArchivo linkArchivo)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != linkArchivo.idLinkArchivo)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(linkArchivo).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!LinkArchivoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/LinkArchivoes
+        [ResponseType(typeof(LinkArchivo))]
+        public IHttpActionResult PostLinkArchivo(LinkArchivo linkArchivo)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.LinkArchivo.Add(linkArchivo);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = linkArchivo.idLinkArchivo }, linkArchivo);
+        }
+
+        // DELETE: api/LinkArchivoes/5
+        [ResponseType(typeof(LinkArchivo))]
+        public IHttpActionResult DeleteLinkArchivo(int id)
+        {
+            LinkArchivo linkArchivo = db.LinkArchivo.Find(id);
+            if (linkArchivo == null)
+            {
+                return NotFound();
+            }
+
+            db.LinkArchivo.Remove(linkArchivo);
+            db.SaveChanges();
+
+            return Ok(linkArchivo);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool LinkArchivoExists(int id)
+        {
+            return db.LinkArchivo.Count(e => e.idLinkArchivo == id) > 0;
+        }
+    }
+}
